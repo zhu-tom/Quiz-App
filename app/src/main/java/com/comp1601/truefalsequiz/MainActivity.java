1a7ba8d95d2f32a5e8244e070d3c5a3b510ba4c2
zhu-tom <zhutom01@gmail.com>
2020-01-30 15:47:17 -0500

    add pagination

--- app/src/main/java/com/comp1601/truefalsequiz/MainActivity.java
+++ app/src/main/java/com/comp1601/truefalsequiz/MainActivity.java
@@ -2,10 +2,13 @@ package com.comp1601.truefalsequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 import android.widget.TextView;
 
@@ -17,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private Button mPrevButton;
     private Button mSubmitButton;
     private ArrayList<Question> mQuestions;
+    private ArrayList<Button> mPageButtons;
     private int[] mUserAnswers;
     private TextView mQuestionTextView;
     private int mCurrentQuestionIndex = 0;
@@ -25,11 +29,14 @@ public class MainActivity extends AppCompatActivity {
     private Button mCButton;
     private Button mDButton;
     private Button mEButton;
+    private Resources mRes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRes = getResources();
 
         mPrevButton = findViewById(R.id.prev_button);
         mNextButton = findViewById(R.id.next_button);
@@ -40,16 +47,17 @@ public class MainActivity extends AppCompatActivity {
             for (int i = 0; i < mQuestions.size(); i++) {
                 userAnswerID = mUserAnswers[i];
                 if (userAnswerID != 0) { // user answer is non-empty
-                    String userAnswer = ((Button) findViewById(userAnswerID)).getTag().toString();
+                    String userAnswer = (findViewById(userAnswerID)).getTag().toString();
                     String correctAnswer = mQuestions.get(i).getAnswer();
 
                     if (userAnswer.equals(correctAnswer)) numCorrect++; // compare user and actual answer
                 }
+                unselectButton(mUserAnswers[i]);
+                mPageButtons.get(i).setBackgroundTintList(mRes.getColorStateList(R.color.basic_button));
             }
             String toastText = "You got " + numCorrect + "/" + mQuestions.size();
             Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
 
-            unselectButton(mUserAnswers[mCurrentQuestionIndex]); // unselect button
             mUserAnswers = new int[mQuestions.size()];  // reset user answers
         });
         mPrevButton.setOnClickListener(v -> {
@@ -75,14 +83,32 @@ public class MainActivity extends AppCompatActivity {
 
         mQuestionTextView = findViewById(R.id.question_text_view);
 
+        mPageButtons = new ArrayList<>();
         mQuestions = new ArrayList<>();
-        TypedArray rawQuestions = getResources().obtainTypedArray(R.array.questions);
+        TypedArray rawQuestions = mRes.obtainTypedArray(R.array.questions);
+
+        LinearLayout pageButtonsLayout = findViewById(R.id.pages);
+        int screenWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
+        int buttonWidth = screenWidth/rawQuestions.length();
+
         for (int i = 0; i < rawQuestions.length(); i++) {
             int rawQuestionResourceId = rawQuestions.getResourceId(i, 0);
             if (rawQuestionResourceId != 0) {
-                String[] rawQuestion = getResources().getStringArray(rawQuestionResourceId);
+                String[] rawQuestion = mRes.getStringArray(rawQuestionResourceId);
                 mQuestions.add(new Question(rawQuestion));
             }
+
+            // add pagination buttons
+            Button pageButton = new Button(this);
+            pageButton.setLayoutParams(new LinearLayout.LayoutParams(buttonWidth, ViewGroup.LayoutParams.WRAP_CONTENT));
+            pageButton.setText(Integer.toString(i+1));
+            int pageNum = i;
+            pageButton.setOnClickListener(v -> {
+                goToQuestion(pageNum);
+            });
+            pageButtonsLayout.addView(pageButton);
+
+            mPageButtons.add(pageButton);
         }
 
         mUserAnswers = new int[mQuestions.size()];
@@ -96,8 +122,17 @@ public class MainActivity extends AppCompatActivity {
             unselectButton(mUserAnswers[mCurrentQuestionIndex]); // remove coloring from last answer
 
             int id = v.getId();
-            mUserAnswers[mCurrentQuestionIndex] = id; // replace old answer with new
-            selectButton(id); // add coloring to new answer
+
+            if (mUserAnswers[mCurrentQuestionIndex] == id) { // clicked original answer
+                mUserAnswers[mCurrentQuestionIndex] = 0; // set to no answer
+                mPageButtons.get(mCurrentQuestionIndex).setBackgroundTintList(mRes.getColorStateList(R.color.basic_button)); // show no answer
+            } else {
+                if (mUserAnswers[mCurrentQuestionIndex] == 0) { // no previous answer
+                    mPageButtons.get(mCurrentQuestionIndex).setBackgroundTintList(mRes.getColorStateList(R.color.selected_button)); // show answered
+                }
+                mUserAnswers[mCurrentQuestionIndex] = id; // replace old answer with new
+                selectButton(id); // add coloring to new answer
+            }
         }
     };
 
@@ -106,10 +141,10 @@ public class MainActivity extends AppCompatActivity {
 
         mCurrentQuestionIndex = nextQuestionIndex; // change current question to input
         if (mCurrentQuestionIndex >= mQuestions.size()) {
-            mCurrentQuestionIndex = 0; // shift to front
+            mCurrentQuestionIndex = 0; // shift to first
         }
         else if (mCurrentQuestionIndex < 0) {
-            mCurrentQuestionIndex = mQuestions.size()-1; // shift to back
+            mCurrentQuestionIndex = mQuestions.size()-1; // shift to last
         }
         Question currentQuestion = mQuestions.get(mCurrentQuestionIndex);
         mQuestionTextView.setText(currentQuestion.getQuestion()); // change question text
@@ -126,14 +161,14 @@ public class MainActivity extends AppCompatActivity {
     private void unselectButton(int id) {
         if (id != 0) {
             Button toUnselect = findViewById(id);
-            toUnselect.setBackgroundTintList(getResources().getColorStateList(R.color.unselected_button));
+            toUnselect.setBackgroundTintList(mRes.getColorStateList(R.color.basic_button));
         }
     }
 
     private void selectButton(int id) {
         if (id != 0) {
             Button selectedAnswer = findViewById(id);
-            selectedAnswer.setBackgroundTintList(getResources().getColorStateList(R.color.selected_button));
+            selectedAnswer.setBackgroundTintList(mRes.getColorStateList(R.color.selected_button));
         }
     }
 }
